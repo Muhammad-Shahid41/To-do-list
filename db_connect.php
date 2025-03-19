bb97c39fcb067fe8f4dcb0213583f0197101df4d
Rashid Mohamad <rashid2538@gmail.com>
2025-03-19 20:06:08 +0530

    Remove unused disconnect function and clean up connect function definition

--- db_connect.php
+++ db_connect.php
@@ -8,10 +8,3 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Database connection failed" . $conn->connect_error);
 }
-
-function disconnect(){
-}
-    
-function connect() {
-    // definition for connecting to DB
-}

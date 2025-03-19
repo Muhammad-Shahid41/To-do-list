2f0625c4798d66e763d19a02ef5a64da54bb2ec6
Rashid Mohamad <rashid2538@gmail.com>
2025-03-19 19:55:15 +0530

    DB connection file updated

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,10 @@
 <?php
-$host="localhost";
-$username="root";
-$password=null;
-$dbname= "todo_list";
+$host = "localhost";
+$username = "root";
+$password = null;
+$dbname = "todo_list";
 
-$conn= new mysqli($host,$username,$password,$dbname);
-if($conn->connect_error){
-    die("Database connection failed".$conn->connect_error);
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Database connection failed" . $conn->connect_error);
 }
-
-?>
